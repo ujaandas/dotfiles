841754a8a558a284f046c244653528fbd7bd0e53
Ujaan Das <ujaandas03@gmail.com>
2024-05-02 23:34:57 +0800

    Functionified

--- symlinker.sh
+++ symlinker.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+# Constants
 dotfiles_dir=~/dotfiles
 log_file=~/install_prog_log.txt
 
-# Create symlink for terminal stuff
-if [ -d ~/.config/alacritty ]; then
-    if [ -L ~/.config/alacritty ]; then
-        echo "Alacritty symlink already exists. Skipping..."
-    else
-        echo "Alacritty config already exists. Attempting to merge..."
-        cp -ai ~/.config/alacritty $dotfiles_dir/terminal/alacritty 
+# Symlink + Extras function
+function createSymLink () {
+    # Rename arguments
+    program_name=$1
+    old_loc=$2
+    new_loc=$3
+
+    if ! [ -L $old_loc ]; then
+	if [ -d $old_loc ]; then
+            echo "$program_name config already exists. Attempting to merge..."
+            cp -ai $old_loc $new_loc 
+        else
+	    echo "$program_name config does no t exist yet. Copying directly from dotfiles..."
+	fi
+	ln -sf $new_loc ~/.config
+	return 1
     fi
-else
-    echo "Alacritty config does not exist yet. Copying directly from dotfiles..."
-fi
-ln -sf $dotfiles_dir/terminal/alacritty ~/.config
+    echo "$program_name symlink already exists. Skipping..."
+    return 0
+}
+
+createSymLink "Alacritty" ~/.config/alacritty ~/dotfiles/terminal/alacritty
