f0a54e52fb5504c65d198a667c27aff45b444327
Ujaan Das <ujaandas03@gmail.com>
2024-05-03 14:41:41 +0800

    Added nvim config + tweaked symlinker

--- symlinker.sh
+++ symlinker.sh
@@ -5,24 +5,30 @@ dotfiles_dir=~/dotfiles
 log_file=~/install_prog_log.txt
 
 # Symlink + Extras function
-function createSymLink () {
-    # Rename arguments
-    program_name=$1
-    old_loc=$2
-    new_loc=$3
+function createSymLink() {
+	# Rename arguments
+	program_name=$1
+	old_loc=$2
+	new_loc=$3
 
-    if ! [ -L $old_loc ]; then
-	if [ -d $old_loc ]; then
-            echo "$program_name config already exists. Attempting to merge..."
-            cp -ai $old_loc $new_loc 
-        else
-	    echo "$program_name config does no t exist yet. Copying directly from dotfiles..."
+	if [ -L $old_loc ]; then
+		if [ -e $old_loc ]; then
+			echo "$program_name has a good link, skipping..."
+		else
+			echo "$program_name has a broken link, re-establishing..."
+			ln -sfn $new_loc ~/.config/
+		fi
+	elif [ -e $old_loc ]; then
+		echo "$program_name exists in $old_loc but is not linked, creating a new link..."
+		cp -ai $old_loc $(dirname "$new_loc")
+		rm -rfi $old_loc
+		ln -sf $new_loc ~/.config/
+	else
+		echo "$program_name not found in $old_loc, creating a new link..."
+		ln -sf $new_loc ~/.config/
 	fi
-	ln -sf $new_loc ~/.config
-	return 1
-    fi
-    echo "$program_name symlink already exists. Skipping..."
-    return 0
+
 }
 
 createSymLink "Alacritty" ~/.config/alacritty ~/dotfiles/terminal/alacritty
+createSymLink "Nvim" ~/.config/nvim ~/dotfiles/apps/nvim
